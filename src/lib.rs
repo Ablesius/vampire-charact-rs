853b1fd69d74076006e79eab48528058ee36faaa
Lexi Blesius <Ablesius@users.noreply.github.com>
2024-05-17 21:58:51 +0200

    Add filtering for jsons in dir
    Co-Authors:
    1. Carnagion#5942 @ Rust Prog Lang Community Discord.
    2. harudagondi#1480.

---
+++ src/lib.rs
@@ -0,0 +1,84 @@
+use std::{path::{Path, PathBuf}, io, fs};
+use std::error::Error;
+
+pub struct Config {
+    pub dir_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() <= 1 {
+            return Err("Needs one more argument!");
+        }
+        else if args.len() > 2 {
+            dbg!("{args}");
+            return Err("Only one argument allowed at this time!");
+        }
+
+        let file_path = args[1].clone();
+        Ok(Config{ dir_path: file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // we start by just printing the json files found
+    let paths = json_paths(&config.dir_path)?;
+    println!("{:#?}", paths);
+    Ok(())
+}
+
+
+// pub fn list_jsons(dir_path: impl AsRef<std::path::Path> ) -> Vec<PathBuf>{
+//     let mut v: Vec<PathBuf> = Vec::new();
+//     let dir = std::fs::read_dir(&dir_path).expect("dir path should be readable!");
+//     for entry in dir {
+//         let path_ref = std::path::Path::new(entry);
+//         let extension = path_ref
+//             .extension()
+//             .unwrap_or_else(|err| eprintln!("Error finding extension for file {}: {}", entry, err));
+//         if extension == "json" {
+//             v.push(path_ref.into());
+//         }
+//     }
+//     v
+// }
+
+
+/// Iterate over a directory and find json files
+/// that we use as character "sheets".
+fn json_paths(dir: impl AsRef<Path>) -> io::Result<Vec<PathBuf>> {
+    // Code originally written by Carnagion#5942 @ Rust Prog Lang Community Discord.
+    // Explanations by harudagondi#1480.
+    // `fs::read_dir` return a result of `ReadDir`, which is an iterator of `Result<DirEntry, Error>`.
+    fs::read_dir(dir)?
+        // a `filter_map` is a `filter` and a `map` combined, as indicated in the name. the filtering mechanism is done through returning an `Option`: `Some` if you want to keep the value, `None` to discard it.
+        .filter_map(
+            |entry|
+                // `entry.map` here means it calls `Result::map`, which is essentially just turning `Result<T, E>` to `Result<U, E>` or in other words, mapping the success value to a different value (with possibly a different type)
+                entry.map(|entry| {
+                    // inside the `map`, carnagion gets the path of the directory, then its extension, then returns a boolean whether or not the `extension()` returned a Some with a value of "json". `then_some` just turns a boolean into a `Some` with the value given if it is true, `None` otherwise
+            let path = entry.path();
+            let is_json = path.extension().is_some_and(|ext| ext == "json");
+            is_json.then_some(path)
+        // going back, `entry.map` in this context turns a `Result<DirEntry, Error>` into `Result<Option<PathBuf>, Error>`. however, `filter_map` wants an `Option`. `transpose` just turns the result inside out so it becomes `Option<Result<PathBuf, Error>>`
+        }).transpose()
+        )
+        .collect()
+}
+
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     #[test]
+//     fn sample_char_dir_three_results() {
+//         let dir = "tests/sample_character_dir";
+//         let expected_jsons = vec![
+//             std::path::PathBuf "tests/sample_character_dir/sample_char.json",
+//             "tests/sample_character_dir/sample_char_2.json",
+//             "tests/sample_character_dir/sample_char_3.json"
+//         ];
+//         let results = json_paths(&dir).expect("test dir should contain tests");
+//         assert_eq!(expected_jsons, results);
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use vampire_charact_rs::Config;
+use std::env;
+use std::process;
+
+/// Vampire-Charact-rs
+///
+/// A Vampire: The Masquerade Character Manager written in Rust
+///
+/// Usage:
+/// `vampire-charact-rs FOLDER`
+/// will list characters that are found in that folder.
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = vampire_charact_rs::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
