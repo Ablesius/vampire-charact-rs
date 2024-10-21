24ef5af03bfd59acb7fef2b3507f8b097699446d
Lexi Blesius <Ablesius@users.noreply.github.com>
2024-10-21 17:39:19 +0200

    refactor[character]: delete deprecated code

--- src/character/skills.rs
+++ src/character/skills.rs
@@ -35,40 +35,6 @@ pub struct Skills {
     pub technology: (u8, Option<String>),
 }
 
-// impl Default for Skills {
-//     fn default() -> Self {
-//         Self {
-//             athletics: (0, None),
-//             brawl: (0, None),
-//             craft: (0, None),
-//             drive: (0, None),
-//             firearms: (0, None),
-//             larceny: (0, None),
-//             melee: (0, None),
-//             stealth: (0, None),
-//             survival: (0, None),
-//             animal_ken: (0, None),
-//             etiquette: (0, None),
-//             insight: (0, None),
-//             intimidation: (0, None),
-//             leadership: (0, None),
-//             performance: (0, None),
-//             persuasion: (0, None),
-//             streetwise: (0, None),
-//             subterfuge: (0, None),
-//             academics: (0, None),
-//             awareness: (0, None),
-//             finance: (0, None),
-//             investigation: (0, None),
-//             medicine: (0, None),
-//             occult: (0, None),
-//             politics: (0, None),
-//             science: (0, None),
-//             technology: (0, None),
-//         }
-//     }
-// }
-
 /// We can use this to do something like
 /// let skill: Skill = Skill::Brawl/* say you get this from user input ... */;
 /// character.skills\[brawl] += 1;
